0be589f4d4fee0699dbb062ff9900ad403d66125
KristianMH <krillemh@gmail.com>
2015-07-09 16:33:58 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+var requireDir = require("require-dir");
+var routes = requireDir("routes");
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,10 +10,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.render('pages/index', {title :"Testing something"});
 });
 
+app.use("/about",routes.about);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
